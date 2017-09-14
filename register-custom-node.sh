a1750543bb1e4b8f6ad4f9f0383318bccff3d830
ambarish2012 <achitnis2012@gmail.com>
2017-09-14 09:54:09 -0700

    Create register-custom-node.sh

---
+++ register-custom-node.sh
@@ -0,0 +1,39 @@
+#! /bin/bash -e
+
+# parameters passed in
+API_TOKEN=$1
+SUBSCRIPTION_ID=$2
+
+# value must match file in https://github.com/Shippable/node/tree/master/scripts
+# specify the  Docker version and OS version that you want and look at the link above
+# to see the supported matrix.
+OS_DOCKER=ubu_16.04_docker_1.13.sh
+
+# retrieve AWS instance id and external IP address for instance
+FRIENDLY_NAME=$(wget -q -O - http://instance-data/latest/meta-data/instance-id)
+IP_ADDRESS=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
+# register the new node with Shippable via POST route
+export RESPONSE=$(curl --request POST \
+  --url https://api.shippable.com/clusterNodes \
+  --header "authorization: apiToken $API_TOKEN" \
+  --header "cache-control: no-cache" \
+  --header "content-type: application/json" \
+  --data "{\"subscriptionId\": \"$SUBSCRIPTION_ID\",\"friendlyName\": \"$FRIENDLY_NAME\",\"location\": \"$IP_ADDRESS\",\"nodeInitScript\": \"$OS_DOCKER\",\"initializeSwap\": false,\"nodeTypeCode\": 7000,\"isShippableInitialized\": false}")
+
+# extract the cluster node id from the response
+CLUSTER_NODE_ID=$(echo $RESPONSE | jq -r '.id')
+
+# download the initialization script from Shippable for this node
+if [[ ! -f shipInitNode.sh ]]; then
+  curl -o ./shipInitNode.sh --request GET https://api.shippable.com/clusterNodes/$CLUSTER_NODE_ID/initScript \
+    --header "authorization: apiToken $API_TOKEN" \
+    --header "cache-control: no-cache" \
+    --header "content-type: application/json"
+fi;
+
+# make the init script executable
+sudo chmod +x shipInitNode.sh
+
+# run the init script
+sudo ./shipInitNode.sh
