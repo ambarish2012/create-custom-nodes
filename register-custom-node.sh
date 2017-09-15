e34ef630f95871dcb182cd41df124f31d9f38c17
ambarish2012 <achitnis2012@gmail.com>
2017-09-14 19:49:25 -0700

    Update register-custom-node.sh

--- register-custom-node.sh
+++ register-custom-node.sh
@@ -3,6 +3,7 @@
 # parameters passed in
 API_TOKEN=$1
 SUBSCRIPTION_ID=$2
+FRIENDLY_NAME=$3
 
 # value must match file in https://github.com/Shippable/node/tree/master/scripts
 # specify the  Docker version and OS version that you want and look at the link above
@@ -13,17 +14,13 @@ OS_DOCKER=ubu_16.04_docker_1.13.sh
 sudo apt-get update
 sudo apt-get -f install jq
 
-# retrieve AWS instance id and external IP address for instance
-FRIENDLY_NAME=$(wget -q -O - http://instance-data/latest/meta-data/instance-id)
-IP_ADDRESS=$(dig +short myip.opendns.com @resolver1.opendns.com)
-
 # register the new node with Shippable via POST route
 export RESPONSE=$(curl --request POST \
   --url https://api.shippable.com/clusterNodes \
   --header "authorization: apiToken $API_TOKEN" \
   --header "cache-control: no-cache" \
   --header "content-type: application/json" \
-  --data "{\"subscriptionId\": \"$SUBSCRIPTION_ID\",\"friendlyName\": \"$FRIENDLY_NAME\",\"location\": \"$IP_ADDRESS\",\"nodeInitScript\": \"$OS_DOCKER\",\"initializeSwap\": false,\"nodeTypeCode\": 7000,\"isShippableInitialized\": false}")
+  --data "{\"subscriptionId\": \"$SUBSCRIPTION_ID\",\"friendlyName\": \"$FRIENDLY_NAME\",\"location\": \"1.1.1.1\",\"nodeInitScript\": \"$OS_DOCKER\",\"initializeSwap\": false,\"nodeTypeCode\": 7000,\"isShippableInitialized\": false}")
 
 # extract the cluster node id from the response
 CLUSTER_NODE_ID=$(echo $RESPONSE | jq -r '.id')
